079a5821477524da767204f4a9231a51bc8bb7aa
petrustris <petrustris@gmail.com>
2017-04-16 20:07:15 +0200

    Dado.py

--- Dado.py
+++ Dado.py
@@ -1,1 +1,15 @@
+!# /usr/bin/python
 
+"""
+ Este programa simula la tirada de un dado.
+ Utiliza una función, aunque más adelante posiblemente sea una clase.
+"""
+
+import random
+
+#Función dado
+def Dado(min = 1, max):
+    #Calcula un valor aleatorio entre min y max. Normalmente es necesario
+    #sólo dar el valor máximo, pero también puedes dar el valor mínimo.
+    #Ejemplos: Dado(20), un dado de 20 caras
+    return random.randint(min, max);
