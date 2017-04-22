4d95f4246f3de3b0775d293f03b497c3fc9c55a0
petrustris <petrustris@gmail.com>
2017-04-22 18:19:56 +0200

    Añado las recetas para tirar las características y  sus valores fraccionales, así como un inicio del proceso

---
+++ CaracteristicasFraccionales.py
@@ -0,0 +1,29 @@
+"""
+Añadimos los valores fraccionales a las características
+"""
+
+#Tirar las características básicas con los valores fraccionales
+
+#Creamos un dado virtual
+import random
+def Dado (max, min = 1):
+    """Calcula un valor aleatorio entre min y max. Normalmente es necesario
+    sólo dar el valor máximo, pero también puedes dar el valor mínimo.
+    Ejemplos: Dado(20), un dado de 20 caras; Dado(7,2), un dado entre 2 y 7"""
+    return random.randint(min, max);
+
+def tirada_fraccional():
+    """Devuelve el valor fraccional en percentiles de cada característica"""
+    lanzamientosD100 = [] #inicializamos una lista vacía
+    for i in zip(caract_nomb): #iteramos en un bucle 6 veces,  1 por cada característica
+        lanzamientosD100.append(Dado(100)) #en cada iteración, lanzamos 1D100, y lo añadimos a la lista
+    return lanzamientosD100  #devuelve una lista con 6 tiradas de D100
+
+val_fraccionales = tirada_fraccional()
+
+# #Tiramos el D100 una vez por cada característica
+# for caract in caract_nomb:
+#     val_fraccionales.append(tirada_fraccional())
+
+#Creamos un diccionario con las características y sus valores fraccionales
+caracteristicas_fracc = dict((x, y) for (x, y) in zip(caract_nomb, val_fraccionales))
